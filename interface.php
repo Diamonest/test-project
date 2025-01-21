3a6e1ac30cfc73b4ccff0af2b2f4febab229fd61
Anton_Chervoniy <anton.chervonyy@mail.ru>
2025-01-21 14:27:26 +0300

    feat: add function to interface

--- interface.php
+++ interface.php
@@ -4,8 +4,8 @@ interface IShape{
     public function getArea();
 }
 interface IMusicAlbum{
-    
+    public function play_track($track_number);
 }
 interface IEmployee{
-
+    public function get_total_salary();
 }
