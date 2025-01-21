ba206161bef9cfc3fba961c56bdcbee989ff2f03
Anton_Chervoniy <anton.chervonyy@mail.ru>
2025-01-21 09:52:57 +0300

    resolve conflict

--- newClass.php
+++ newClass.php
@@ -133,3 +133,4 @@ print_r("Бонус:", $employee->get_bonus());
 print_r("Итого начислено:", $employee->get_total_salary());
 print("Solve conflict git rebase 2");
 print_r("Solve another conflict");
+print_r("solve confilcts many many many many many many");
